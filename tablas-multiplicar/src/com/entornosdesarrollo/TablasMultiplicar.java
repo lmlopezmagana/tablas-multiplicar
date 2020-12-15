534dcd6b3ab1d2ea34aaabef3be499c27e288333
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2020-12-15 13:14:51 +0100

    Tablas del 1 al 10 con While

--- tablas-multiplicar/src/com/entornosdesarrollo/TablasMultiplicar.java
+++ tablas-multiplicar/src/com/entornosdesarrollo/TablasMultiplicar.java
@@ -5,10 +5,16 @@ public class TablasMultiplicar {
 	public static void main(String[] args) {
 		
 		
-		for(int i = 1; i <= 10; i++) {
-			
+		int i = 1;
+		while (i <= 10) {
+			int j = 1;
+			while (j <= 10) {
+				System.out.println(i+"*"+j+"="+i*j);
+				j++;
+			}
+			i++;
+			System.out.println("");
 		}
-
 	}
 
 }
